8aef14b783a6987dd7dda3f7cdafbe9567a8786c
DrNossberg <titou210@hotmail.fr>
2021-01-28 17:41:51 +0100

    [Modify] Maze, move most of the function to MazeGenerator
    -
    -

--- src/Maze.java
+++ src/Maze.java
@@ -13,89 +13,36 @@ import java.util.ArrayList;
 import java.awt.geom.*;
 
 public class Maze {
-    private int width, height;
-    char[][] lab; // '#' for wall and '.' for path
+	private int width;
+	private int height; // DCL52-J. Do not declare more then one variable per declaration
+	char[][] tab;
 
-    /**
-     * Create new Labythin object with given width and height and full of walls
-     */
-    public Maze(int width, int height) {
-        this.width = width;
-        this.height = height;
+	public Maze(int width, int height, int default_char) {
+		this.width = width;
+		this.height = height;
 
-        // Initialization of the labyrinth
-        this.lab = new char[this.height][this.width];
+		this.tab = new char[this.height][this.width];
+		for (int y = 0; y < this.height; y++) // row by row
+				tab[y].fill(width, default_char);
+	}
 
-        for (int y = 0; y < this.height; y++) { // row by row
-            for (int x = 0; x < this.width; x++) { // column by column
-                lab[y][x] = '#';
-            }
-        }
-    }
+	/**
+	 * Change a gived position
+	 * @param node
+	 */
+	public void change(Point2D node, int value) {
+		this.tab[node.getY()][node.getX()] = value; //
+	}
 
-    /**
-     * Change a slot from wall to path
-     * @param node
-     */
-    public void changeToPath(Point2D node) {
-        this.lab[(int) node.getY()][(int) node.getX()] = '.';
-    }
+	public void display() { // color option for colored output ?
+		for (int y = 0; y < this.height; y++) { // row by row
+			for (int x = 0; x < this.width; x++) { // column by column
+				System.out.print(this.tab[y][x]);
+			System.out.println('');
+			}
+		}
+	}
 
-    public String printLab() {
-        String res = "";
-
-
-        for (int y = 0; y < this.height; y++) { // row by row
-            for (int x = 0; x < this.width; x++) { // column by column
-                res += this.lab[y][x]; 
-            }
-            res += ("\n");
-        }
-        return (res);
-    }
-
-    /**
-     * Select a random unvisited neighbor of the active Node
-     * @param visitedNodes array of already visited nodes
-     * @param activeNode
-     * @return random neighbor (takes into account if none exists)
-     */
-    public Point2D randomNeighbor(ArrayList<Point2D> visitedNodes, Point2D activeNode) {
-        Point2D res;
-        ArrayList<Point2D> unvisitedNeighbors = this.findUnvisitedNeighbors(visitedNodes, activeNode);
-        // define selected unvisited neighbor
-        if (unvisitedNeighbors.size() > 0)
-            res = unvisitedNeighbors.get(Main.R.nextInt(unvisitedNeighbors.size()));
-        else // none exists
-            res = new Point2D.Double(Main.DEFAULT_NODE,Main.DEFAULT_NODE);
-        
-        return res;
-    }
-
-    private ArrayList<Point2D> findUnvisitedNeighbors(ArrayList<Point2D> visitedNodes, Point2D activeNode) {
-        // create an array with all unvisited neighbors
-        ArrayList<Point2D> unvisitedNeighbors = new ArrayList<>();
-        double x = activeNode.getX();
-        double y = activeNode.getY();
-
-        // fill array of unvisited neighbors with existing nodes
-        if (x > 0 && !visitedNodes.contains(new Point2D.Double(x-2, y))) // x = 0 -> left border
-            unvisitedNeighbors.add(new Point2D.Double(x-2, y));
-
-        if (x < this.width-1 && !visitedNodes.contains(new Point2D.Double(x+2, y))) // x = width-1 -> right border
-            unvisitedNeighbors.add(new Point2D.Double(x+2,y));
-
-        if (y > 0 && !visitedNodes.contains(new Point2D.Double(x, y-2))) // y = 0 -> top border
-            unvisitedNeighbors.add(new Point2D.Double(x, y-2));
-
-        if (y < this.height-1 && !visitedNodes.contains(new Point2D.Double(x, y+2))) // y = height-1 -> bottom border
-            unvisitedNeighbors.add(new Point2D.Double(x, y+2));
-
-        return (unvisitedNeighbors);
-    }
-
-    //#region getter
-    public int getWidth() { return this.width; }
-    public int getHeight() { return this.height; }
-    //#endregion
+	public int getWidth() {     return this.width;  }
+	public int getHeight() {    return this.height; }
 }
--- src/Parser.java
+++ src/Parser.java
@@ -8,3 +8,28 @@
 ** File description :
 **    Parser of the Labythin project 
 */
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+
+
+class Parser {
+    @Option(names = {"-h", "--help"}, usageHelp= true, description = "display this help and exit")
+    boolean help;
+    @Option(names = {"-x", "-w", "--width"},  description = "width of the maze to create. By default : ${DEFAULT-VALUE}") //paramLabel = "width", ?
+    double width = 50;
+    @Option(names = {"-y", "-h", "--height"},  description = "height of the maze to create. By default : ${DEFAULT-VALUE}") //paramLabel = "height", ?
+    double height = 50;
+    @Option(names = {"-v", "--verbose"}, description = "verbose mode of display")
+    boolean verbose;
+    @Option(names = {"-c", "--color"}, description = "color the output")
+    boolean color;
+    @Option(names = {"-f", "--file"}, description = "file to read the maze from")
+    File f_intput;
+    @Option(names = {"-o", "--output"}, description = "file to output the maze to")
+    File f_output;
+}
